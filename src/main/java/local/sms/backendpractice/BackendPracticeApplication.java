ac9325c8c9277ac0b721823f20d473670ddeb05f
seanmx96 <seanmx96@users.noreply.github.com>
2020-12-11 15:03:37 -0600

    first commit

---
+++ src/main/java/local/sms/backendpractice/BackendPracticeApplication.java
@@ -0,0 +1,16 @@
+package local.sms.backendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPracticeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BackendPracticeApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/local/sms/backendpractice/BackendPracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package local.sms.backendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPracticeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
